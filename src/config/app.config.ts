1d397d2277ab920f8430f3137daceb2578810391
reasonly8 <reasonly8@sina.com>
2024-11-29 09:16:24 +0800

    feat: add router

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,7 +3,7 @@ import { registerAs } from '@nestjs/config';
 export default registerAs('app', () => {
   return {
     port: Number(process.env.APP_PORT || 3000),
-    globalPrefix: process.env.APP_GLOBAL_PREFIX || '',
+    globalPrefix: process.env.APP_GLOBAL_PREFIX || '/',
     isDev: process.env.NODE_ENV === 'development',
     isProd: process.env.NODE_ENV === 'production',
   };
